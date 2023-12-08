671e12ca71ee71d738e216f764097cacfe23c355
CraigCharlton <charlton87@live.com>
2023-12-08 21:15:09 +0000

    Added extras. Updated styles.

--- src/App.js
+++ src/App.js
@@ -28,31 +28,43 @@ function App() {
       <main>
         <div className="container">
         {quotes.map((quote) => (
-          <div className="quote"  key={quote.firstName}>
+          <div className="quote" key={quote.firstName}>
             <div className="box details">
               <h2>Hey {quote.firstName},</h2>
               <p>Here is your quote for Royal & Sun Alliance, {quote.address1}, {quote.address2}</p>
               <p>Quote reference: {quote.quoteRef}</p>
               <p>Covers starts on {quote.startDate}.</p>
             </div>
-            {isMonthly && (  
-              <div className="box price">                     
+            {isMonthly && (
+              <div className="box price">
                 <span className="currency">£{quote.monthlyPrice}</span>
-                <p>per month</p>
+                <p className="per-date">per month</p>
                 <p>This price includes Insurance Premium Tax at the current rate. No charge for paying monthly.</p>
-                <button onClick={() => setIsMonthly(false)}>Switch to annual</button>
+                <button className="quote-button" onClick={() => setIsMonthly(false)}>Switch to annual</button>
               </div>
             )}
-            {!isMonthly && (  
-              <div className="box price">                     
+            {!isMonthly && (
+              <div className="box price">
                 <span className="currency">£{quote.annualPrice}</span>
-                <p>per year</p>
+                <p className="per-date">per year</p>
                 <p>This price includes Insurance Premium Tax at the current rate. No charge for paying monthly.</p>
-                <button onClick={() => setIsMonthly(true)}>Switch to monthly</button>
+                <button className="quote-button" onClick={() => setIsMonthly(true)}>Switch to monthly</button>
               </div>
             )}
           </div>
         ))}
+        <span className="heading2"><h2>Tailor your cover with our optional extra</h2></span>
+
+        <div className="extras">
+          {addons.map((addon) => (
+            <div className="box" key={addon.title}>
+              <h3>{addon.title}</h3>
+              <span className="box-price">£{addon.monthlyPrice} per month</span>
+              <p>{addon.text}</p>
+              <button className="extras-button">Select this extra</button>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
     </div>
